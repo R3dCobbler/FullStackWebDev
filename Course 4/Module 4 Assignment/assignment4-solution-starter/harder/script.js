e9846c8ad0a28dff4eb3956b8057151be21365e7
R3dCobbler <r3dcobbler@hotmail.co.uk>
2018-11-15 16:56:29 +0100

    Final submission

--- Course 4/Module 4 Assignment/assignment4-solution-starter/harder/script.js
+++ Course 4/Module 4 Assignment/assignment4-solution-starter/harder/script.js
@@ -52,6 +52,7 @@ for (var i in names) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+  
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
